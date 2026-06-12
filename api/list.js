ca0981d0f9194075f98c63385ad3a14d91174145
𝐑𝐎𝐂𝐊𝐘 𝐂𝐇𝐎𝐖𝐃𝐇𝐔𝐑𝐘 <messengergoatbot320@gmail.com>
2026-06-12 13:56:07 +0600

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,19 +1,21 @@
-const couples = [
-  {
-    boy: "https://i.imgur.com/14uLcSi.jpeg",
-    girl: "https://i.imgur.com/7eKVQqW.jpeg"
-  },
-  {
-    boy: "https://i.imgur.com/rg22DqL.jpeg",
-    girl: "https://i.imgur.com/dCRdzhr.jpeg"
-  },
-  {
-    boy: "https://i.imgur.com/qXIn00k.jpeg",
-    girl: "https://i.imgur.com/JJ2Imdp.jpeg"
-  }
-];
+const axios = require("axios");
 
-module.exports = (req, res) => {
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GITHUB_REPO = "messengergoatbot320-lang/cdp-api";
+const FILE_PATH = "data/couples.json";
+
+module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  return res.status(200).json({ total: couples.length });
+  try {
+    const getFile = await axios.get(
+      `https://api.github.com/repos/${GITHUB_REPO}/contents/${FILE_PATH}`,
+      { headers: { Authorization: `token ${GITHUB_TOKEN}` } }
+    );
+    const couples = JSON.parse(
+      Buffer.from(getFile.data.content, "base64").toString("utf8")
+    );
+    return res.status(200).json({ total: couples.length });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
 };
