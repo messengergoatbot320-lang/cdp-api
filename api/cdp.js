dd54d7d25c3e70436918e0ae7d31edddf8a128cc
𝐑𝐎𝐂𝐊𝐘 𝐂𝐇𝐎𝐖𝐃𝐇𝐔𝐑𝐘 <messengergoatbot320@gmail.com>
2026-06-12 11:54:28 +0600

    Update cdp.js

--- api/cdp.js
+++ api/cdp.js
@@ -1,6 +1,3 @@
-const axios = require("axios");
-
-// তোমার নিজের image list এখানে রাখো
 const couples = [
   {
     boy: "https://i.imgur.com/14uLcSi.jpeg",
@@ -14,16 +11,10 @@ const couples = [
     boy: "https://i.imgur.com/qXIn00k.jpeg",
     girl: "https://i.imgur.com/JJ2Imdp.jpeg"
   }
-  // আরো add করো
 ];
 
 module.exports = (req, res) => {
-  const { path } = req.query;
-
-  if (req.url.includes("/list")) {
-    return res.json({ total: couples.length });
-  }
-
+  res.setHeader("Access-Control-Allow-Origin", "*");
   const random = couples[Math.floor(Math.random() * couples.length)];
-  return res.json(random);
+  return res.status(200).json(random);
 };
