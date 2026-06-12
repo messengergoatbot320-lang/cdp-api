f7d84b00a257e53a95ad333be2aed38ff1310f3f
𝐑𝐎𝐂𝐊𝐘 𝐂𝐇𝐎𝐖𝐃𝐇𝐔𝐑𝐘 <messengergoatbot320@gmail.com>
2026-06-12 13:55:18 +0600

    Update add.js

--- api/add.js
+++ api/add.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 
-const IMGUR_CLIENT_ID = "তোমার_imgur_client_id";
-const GITHUB_TOKEN = "তোমার_github_token";
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const GITHUB_REPO = "messengergoatbot320-lang/cdp-api";
 const FILE_PATH = "data/couples.json";
 
@@ -23,21 +22,6 @@ module.exports = async (req, res) => {
       return res.status(400).json({ error: "boyUrl and girlUrl required" });
     }
 
-    // Imgur এ upload
-    const uploadToImgur = async (imageUrl) => {
-      const response = await axios.post("https://api.imgur.com/3/image", {
-        image: imageUrl,
-        type: "url"
-      }, {
-        headers: { Authorization: `Client-ID ${IMGUR_CLIENT_ID}` }
-      });
-      return response.data.data.link;
-    };
-
-    const imgurBoy = await uploadToImgur(boyUrl);
-    const imgurGirl = await uploadToImgur(girlUrl);
-
-    // GitHub থেকে couples.json পড়ো
     const getFile = await axios.get(
       `https://api.github.com/repos/${GITHUB_REPO}/contents/${FILE_PATH}`,
       { headers: { Authorization: `token ${GITHUB_TOKEN}` } }
@@ -47,14 +31,15 @@ module.exports = async (req, res) => {
       Buffer.from(getFile.data.content, "base64").toString("utf8")
     );
 
-    currentContent.push({ boy: imgurBoy, girl: imgurGirl });
+    currentContent.push({ boy: boyUrl, girl: girlUrl });
 
-    // GitHub এ save করো
     await axios.put(
       `https://api.github.com/repos/${GITHUB_REPO}/contents/${FILE_PATH}`,
       {
         message: "Add new couple DP via bot",
-        content: Buffer.from(JSON.stringify(currentContent, null, 2)).toString("base64"),
+        content: Buffer.from(
+          JSON.stringify(currentContent, null, 2)
+        ).toString("base64"),
         sha: getFile.data.sha
       },
       { headers: { Authorization: `token ${GITHUB_TOKEN}` } }
@@ -62,8 +47,8 @@ module.exports = async (req, res) => {
 
     return res.status(200).json({
       success: true,
-      boy: imgurBoy,
-      girl: imgurGirl,
+      boy: boyUrl,
+      girl: girlUrl,
       total: currentContent.length
     });
 
